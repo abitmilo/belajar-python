6b3a077290d16238dffc1f3836542414c7585dc3
abit <116137005+abitmilo@users.noreply.github.com>
2025-12-24 11:21:09 +0700

    day3-input-output

---
+++ data_diri.py
@@ -0,0 +1,7 @@
+nama = input("Masukkan nama :")
+umur = input("Berapa umur anda :")
+hobi = input("Apa hobi anda :")
+
+print('halo',nama)
+print('umur kamu adalah',umur)
+print('hobi kamu adalah', hobi)
---
+++ kalkulator.py
@@ -0,0 +1,6 @@
+angka1 = int(input("Masukkan angka pertama : "))
+angka2 = int(input("Masukkan angka kedua : "))
+
+hasil = angka1 + angka2
+
+print("hasil : ", hasil)

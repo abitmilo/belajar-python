4a5bc784fd95410217ffa01c0d0434f731f547bf
abit <116137005+abitmilo@users.noreply.github.com>
2025-12-28 12:46:09 +0700

    for and while

---
+++ attendance_robot.py
@@ -0,0 +1,21 @@
+#tugas 1
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(f"Bilangan {i} adalah Genap")
+    else:
+        print(f"Bilangan {i} adalah ganjil")
+
+
+#tugas 2        
+password_benar = "python1234"
+
+while True:
+    input_user = input("Masukkan password ")
+    
+    if input_user == password_benar:
+        print("Login Berhasil! Selamat datang")
+        break
+    else:
+        print("Password salah! Coba lagi")
+        
+print("Program Selesai")
